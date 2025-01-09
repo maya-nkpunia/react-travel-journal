492e735546749806feacae163ca621d620c86dab
Mayank-p19 <mayankpunia74@gmail.com>
2025-01-09 21:00:50 +0530

    props

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,48 +1,18 @@
-export default function Entry() {
+export default function Entry(props) {
     return (
         <article className="journal-entry">
             <div className="main-container">
             <div className="main-image-container">
-                <img className="main-image" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="mount fuji" />
+                <img className="main-image" src={props.src} alt={props.alt} />
             </div>
             <div className="info-container">
                 <img className="marker-image" src="src\images\marker_map_icon.png" alt="map marker icon" />
-                <span className="country">Japan</span>
-                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google Maps</a>
+                <span className="country">{props.country}</span>
+                <a href={props.googleMapsLink}>View on Google Maps</a>
 
-                <h2 className="entry-title">Mount Fuji</h2>
-                <p className="trip-dates">12 Jan, 2025 - 24 Jan, 2025</p>
-                <p className="entry-text">Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. A pilgrimage site for centuries, it’s considered one of Japan’s 3 sacred mountains, and summit hikes remain a popular activity. Its iconic profile is the subject of numerous works of art, notably Edo Period prints by Hokusai and Hiroshige.</p>
-            </div>
-            </div>
-
-            <div className="main-container">
-            <div className="main-image-container">
-                <img className="main-image" src="src/images/Eiffle-tower.webp" alt="eiffel tower" />
-            </div>
-            <div className="info-container">
-                <img className="marker-image" src="src\images\marker_map_icon.png" alt="map marker icon" />
-                <span className="country">France</span>
-                <a href="https://www.google.com/maps/place/Eiffel+Tower/@48.8581646,2.2929461,17.33z/data=!4m10!1m2!2m1!1seiffel+tower!3m6!1s0x47e66e2964e34e2d:0x8ddca9ee380ef7e0!8m2!3d48.8583701!4d2.2944813!15sCgxlaWZmZWwgdG93ZXJaDiIMZWlmZmVsIHRvd2VykgETaGlzdG9yaWNhbF9sYW5kbWFya-ABAA!16zL20vMDJqODE?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D">View on Google Maps</a>
-
-                <h2 className="entry-title">Eiffel Tower</h2>
-                <p className="trip-dates">17 March, 2025 - 26 March, 2025</p>
-                <p className="entry-text">The Eiffel Tower, an iconic wrought-iron lattice structure, stands on the Champ de Mars in Paris, France. Designed by engineer Gustave Eiffel, it was inaugurated in 1889 as the centerpiece of the World’s Fair, celebrating the 100th anniversary of the French Revolution. Rising to a height of 330 meters (1,083 feet), it was the tallest structure in the world until 1930. Today, it remains one of the most recognizable landmarks globally, attracting millions of visitors each year who enjoy panoramic views of Paris from its three accessible levels.</p>
-            </div>
-            </div>
-
-            <div className="main-container">
-            <div className="main-image-container">
-                <img className="main-image" src="\src\images\geirangerfjord.webp" alt="eiffel tower" />
-            </div>
-            <div className="info-container">
-                <img className="marker-image" src="src\images\marker_map_icon.png" alt="map marker icon" />
-                <span className="country">Norway</span>
-                <a href="https://www.google.com/maps/place/Geirangerfjord/@62.1010599,6.977256,11.71z/data=!4m6!3m5!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817!16zL20vMDZ4eXFi?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D">View on Google Maps</a>
-
-                <h2 className="entry-title">Geirangerfjord</h2>
-                <p className="trip-dates">8 July, 2025 - 21 July, 2025</p>
-                <p className="entry-text">Geirangerfjord, located in Norway, is one of the country’s most famous fjords and a UNESCO World Heritage Site. Known for its stunning natural beauty, the fjord is surrounded by steep mountains, lush greenery, and cascading waterfalls such as the Seven Sisters and the Suitor. It stretches approximately 15 kilometers (9 miles) and is renowned for its deep, blue waters and picturesque landscapes. Visitors can explore the fjord through boat tours, hiking trails, or kayaking, making it a popular destination for nature enthusiasts.Geirangerfjord is a prime example of Norway’s dramatic fjord scenery, offering unforgettable views and serene tranquility.</p>
+                <h2 className="entry-title">{props.title}</h2>
+                <p className="trip-dates">{props.dates}</p>
+                <p className="entry-text">{props.text}</p>
             </div>
             </div>
         </article>
