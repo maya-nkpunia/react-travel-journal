22f2893552694ec293e69f190d8f29a24db20b98
Mayank-p19 <mayankpunia74@gmail.com>
2025-01-10 22:42:47 +0530

    last commit

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -3,16 +3,16 @@ export default function Entry(props) {
         <article className="journal-entry">
             <div className="main-container">
             <div className="main-image-container">
-                <img className="main-image" src={props.img.src} alt={props.img.alt} />
+                <img className="main-image" src={props.entry.img.src} alt={props.entry.img.alt} />
             </div>
             <div className="info-container">
                 <img className="marker-image" src="src\images\marker_map_icon.png" alt="map marker icon" />
-                <span className="country">{props.country}</span>
-                <a href={props.googleMapsLink}>View on Google Maps</a>
+                <span className="country">{props.entry.country}</span>
+                <a href={props.entry.googleMapsLink}>View on Google Maps</a>
 
-                <h2 className="entry-title">{props.title}</h2>
-                <p className="trip-dates">{props.dates}</p>
-                <p className="entry-text">{props.text}</p>
+                <h2 className="entry-title">{props.entry.title}</h2>
+                <p className="trip-dates">{props.entry.dates}</p>
+                <p className="entry-text">{props.entry.text}</p>
             </div>
             </div>
         </article>
